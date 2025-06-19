fa7d31abf7a8eecbb4c427707b922ada2307c8bf
Andrew Mackenzie <andrew@mackenzie-serres.net>
2025-06-19 18:00:18 +0200

    Initial commit - implements the "Homomorphic Hello World"

---
+++ README.md
@@ -0,0 +1,4 @@
+# Homomorphic Encrypted CRDTs
+
+Implements the examples of using Homomorphic Encryption together with CRDTs described in [this article](https://jakelazaroff.com/words/homomorphically-encrypted-crdts/)
+
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use tfhe::prelude::*;
+use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32, ServerKey};
+
+fn main() {
+    let config = ConfigBuilder::default().build();
+
+    // generate client and server keys
+    let (client_key, server_key) = generate_keys(config);
+
+    // generate plaintext
+    let clear_a: u32 = 27;
+    let clear_b: u32 = 128;
+
+    // encrypt plaintext and "send to server"
+    let result = server_compute(
+        server_key,
+        FheUint32::encrypt(clear_a, &client_key),
+        FheUint32::encrypt(clear_b, &client_key),
+    );
+
+    // decrypt the result
+    let decrypted_result: u32 = result.decrypt(&client_key);
+
+    // assert that the result is what we expect
+    assert_eq!(decrypted_result, clear_a + clear_b);
+    println!("Encrypted sum equals unencrypted sum")
+}
+
+fn server_compute(key: ServerKey, cipher_a: FheUint32, cipher_b: FheUint32) -> FheUint32 {
+    set_server_key(key);
+    return cipher_a + cipher_b;
+}
